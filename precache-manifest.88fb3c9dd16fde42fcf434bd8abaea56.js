ee79b75c10a3f2375b2613335573499affe7ae1a
Keshav-016 <kkeshavkumarhigh@gmail.com>
2023-04-13 12:21:26 +0530

    Updates

---
+++ precache-manifest.88fb3c9dd16fde42fcf434bd8abaea56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1660c8dda4e746d85ec4",
+    "url": "/Tesla/static/css/main.f8671f5d.chunk.css"
+  },
+  {
+    "revision": "1660c8dda4e746d85ec4",
+    "url": "/Tesla/static/js/main.1660c8dd.chunk.js"
+  },
+  {
+    "revision": "a2f7525db028d38c2481",
+    "url": "/Tesla/static/js/1.a2f7525d.chunk.js"
+  },
+  {
+    "revision": "9c5479db102c1fa1fe89",
+    "url": "/Tesla/static/js/runtime~main.9c5479db.js"
+  },
+  {
+    "revision": "195bd7ee5f2ce26b6ae84a24a413502f",
+    "url": "/Tesla/index.html"
+  }
+];
